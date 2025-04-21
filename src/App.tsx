498b61315b43fe30d0005f4381770e1826216c61
Carlos Eduardo B. Kessler <kadukessler@gmail.com>
2025-04-21 04:50:40 -0300

    ✨ feat(Heading): adicionar componente Heading com estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "@styles/theme.css";
 import "@styles/global.css";
 
+import { TimerIcon } from "lucide-react";
+
+import { Heading } from "@components/Heading";
+
 export function App() {
   return (
     <>
+      <Heading>
+        Heading
+        <button title="Timer">
+          <TimerIcon />
+        </button>
+      </Heading>
       <h1>My React App</h1>
       <p>This is a simple React application.</p>
     </>
