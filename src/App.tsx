1972daf753947c30c74292fde6b108bcb2818b21
Carlos Eduardo B. Kessler <kadukessler@gmail.com>
2025-04-21 08:41:06 -0300

    ✨ feat(App): atualizar componente App para usar Heading e Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import "@styles/theme.css";
 import "@styles/global.css";
-
-import { TimerIcon } from "lucide-react";
-
-import { Heading } from "@components/Heading";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Heading
-        <button title="Timer">
-          <TimerIcon />
-        </button>
-      </Heading>
-      <h1>My React App</h1>
-      <p>This is a simple React application.</p>
+      <Heading>Kairos</Heading>
+
+      <Container>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+        tempore omnis aperiam eveniet totam architecto molestias incidunt
+        dolorem voluptatibus aspernatur et reiciendis nobis voluptates alias
+        maxime temporibus rerum impedit voluptate.
+      </Container>
+
+      <Container>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+        tempore omnis aperiam eveniet totam architecto molestias incidunt
+        dolorem voluptatibus aspernatur et reiciendis nobis voluptates alias
+        maxime temporibus rerum impedit voluptate.
+      </Container>
     </>
   );
 }
