fccfb5252c80ef9d4b866d454676faa60d7d50bc
Carlos Eduardo B. Kessler <kadukessler@gmail.com>
2025-04-21 21:17:46 -0300

    ✨ feat(Input): adiciona componente Input e integra no formulário do App
    Adiciona um novo componente Input com suporte a rótulos e estilização. O componente foi integrado ao formulário na aplicação, permitindo a entrada de tarefas. Além disso, o estilo do formulário foi atualizado para melhorar a apresentação visual.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "@components/Container";
 import { Menu } from "@components/Menu";
 import { Logo } from "@components/Logo";
 import { CountDown } from "@components/CountDown";
+import { Input } from "@components/Input";
 
 export function App() {
   return (
@@ -19,6 +20,32 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <Input
+              id="inputTask"
+              type="text"
+              labelText="Tarefa"
+              placeholder="Digite Algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
